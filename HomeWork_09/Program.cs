02d3dcf06a7205050e2a675878c9672555599a2d
Aleksey6115 <trunkinam@gmail.com>
2021-09-24 21:54:51 +0600

    First_commit

---
+++ HomeWork_09/Program.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Newtonsoft.Json;
+
+namespace HomeWork_09
+{
+    class Program
+    {
+        static TelegramBotClient bot; // Работа с ботом
+        static List<string> list_file = new List<string>(); // Список ранее загруженных файлов
+        static TelegramOptions telegramoptions = new TelegramOptions(); // Функционал бота
+
+        /// <summary>
+        /// Точка входа в программу
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            // Подключиться к боту
+            string token = File.ReadAllText("");
+            bot = new TelegramBotClient(token);
+
+            // Записать в лист список ранее загруженных файлов
+            TelegramOptions telegram_options = new TelegramOptions();
+            list_file = telegram_options.DeSerialization();
+
+            // Бот получает сообщение
+            bot.OnMessage += Message;
+            bot.StartReceiving();
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Обработчик события OnMessage
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void Message(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Document) // Если отправили документ
+            #region
+            {
+                Console.WriteLine($"User: {e.Message.Chat.FirstName}  |  FileName: {e.Message.Document.FileName}  |  Type: {e.Message.Type}");
+
+                //Сохранить файл на диск
+                try
+                {
+                    telegramoptions.Upload(e.Message.Document.FileId, e.Message.Document.FileName, bot);
+                    list_file.Add(e.Message.Document.FileName);
+                    telegramoptions.Serialization(list_file);
+                    bot.SendTextMessageAsync(e.Message.Chat.Id, "Загрузка прошла успешно!");
+                }
+                catch
+                {
+                    bot.SendTextMessageAsync(e.Message.Chat.Id, "Ошибка загрузки...");
+                }
+            }
+            #endregion
+
+            else if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Photo) // Если отправили фото
+            #region
+            {
+                Console.WriteLine($"User: {e.Message.Chat.FirstName}  |  Type: {e.Message.Type}");
+
+                //Сохранить фото на диск
+                try
+                {
+                    telegramoptions.Upload(e.Message.Photo[3].FileId, $"photo{list_file.Count + 1}", bot);
+                    list_file.Add($"photo{list_file.Count + 1}");
+                    telegramoptions.Serialization(list_file);
+                    bot.SendTextMessageAsync(e.Message.Chat.Id, "Загрузка прошла успешно!");
+                }
+                catch
+                {
+                    bot.SendTextMessageAsync(e.Message.Chat.Id, "Ошибка загрузки...");
+                }
+            }
+            #endregion
+
+            else if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Audio) // Если отправили аудио
+            #region
+            {
+                Console.WriteLine($"User: {e.Message.Chat.FirstName}  |  FileName: {e.Message.Audio.FileName}  |  Type: {e.Message.Type}");
+
+                // Сохранить аудио на диск
+                try
+                {
+                    telegramoptions.Upload(e.Message.Audio.FileId, e.Message.Audio.FileName, bot);
+                    list_file.Add(e.Message.Audio.FileName);
+                    telegramoptions.Serialization(list_file);
+                    bot.SendTextMessageAsync(e.Message.Chat.Id, "Загрузка прошла успешно!");
+                }
+                catch
+                {
+                    bot.SendTextMessageAsync(e.Message.Chat.Id, "Ошибка загрузки...");
+                }
+            }
+            #endregion
+
+            else if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Text) // Если отправили текст
+            #region
+            {
+                Console.WriteLine($"User: {e.Message.Chat.FirstName}  |  Message: {e.Message.Text}  |  Type: {e.Message.Type}");
+
+                if (e.Message.Text.Contains("/file")) // Отправить файл пользователю
+                #region
+                {
+                    if (telegramoptions.GettFileNumber(e.Message.Text) == -1)
+                        bot.SendTextMessageAsync(e.Message.Chat.Id, "К сожалению такого файла не найдено!");
+
+                    else
+                    {
+                        try
+                        {
+                            if (list_file[telegramoptions.GettFileNumber(e.Message.Text)].Contains("."))
+                            telegramoptions.Download(e.Message.Chat.Id.ToString(), 
+                                list_file[telegramoptions.GettFileNumber(e.Message.Text)], bot);
+
+                            // Когда сохраняется фото, то в его имени нет символа "."
+                            else
+                                telegramoptions.DownloadPhoto(e.Message.Chat.Id.ToString(), 
+                                    list_file[telegramoptions.GettFileNumber(e.Message.Text)], bot);
+                        }
+                        catch
+                        {
+                            bot.SendTextMessageAsync(e.Message.Chat.Id, "Ошибка скачивания...");
+                        }
+                    }
+                }
+                #endregion
+
+                else
+                {
+                    switch (e.Message.Text)
+                    {
+                        case "/start":
+                            #region
+                            bot.SendTextMessageAsync(e.Message.Chat.Id, telegramoptions.Start($"{e.Message.Chat.LastName} " +
+                                $"{e.Message.Chat.FirstName}"));
+                            #endregion
+                            break;
+
+                        case "/download_files":
+                            #region
+                            if (list_file.Count == 0)
+                                bot.SendTextMessageAsync(e.Message.Chat.Id, "Список загруженных файлов пуст");
+
+                            else
+                            {
+                                bot.SendTextMessageAsync(e.Message.Chat.Id, telegramoptions.ShowListFilesForDownload(list_file));
+                            }
+                            #endregion
+                            break;
+
+                        case "/list_files":
+                            #region
+                            if (list_file.Count == 0)
+                                bot.SendTextMessageAsync(e.Message.Chat.Id, "Список загруженных файлов пуст");
+
+                            else
+                                bot.SendTextMessageAsync(e.Message.Chat.Id, telegramoptions.ShowListFiles(list_file));
+                            #endregion
+                            break;
+
+                        default:
+                            bot.SendTextMessageAsync(e.Message.Chat.Id, "К сожалению эта команда мне не знакома...");
+                            break;
+                    }
+                }
+            }
+            #endregion
+
+            else
+                bot.SendTextMessageAsync(e.Message.Chat.Id, "К сожалению этот тип файла мне не знаком...");
+        }
+    }
+}
---
+++ HomeWork_09/TelegramOptions.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using Telegram.Bot;
+using System.Net;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace HomeWork_09
+{
+    class TelegramOptions
+    {
+        /// <summary>
+        /// Метод загружает файл на ПК
+        /// </summary>
+        /// <param name="fileId">id файла</param>
+        /// <param name="path">Путь для созранения</param>
+        /// <param name="bot">бот</param>
+        public async void Upload(string fileId, string path, TelegramBotClient bot)
+        {
+            var file = await bot.GetFileAsync(fileId);
+            using (FileStream fs = new FileStream(path, FileMode.Create))
+            {
+                await bot.DownloadFileAsync(file.FilePath, fs);
+            }
+        }
+
+        /// <summary>
+        /// Метод отправляет файл пользователю
+        /// </summary>
+        /// <param name="chatid">id чата</param>
+        /// <param name="path">Путь к файлу</param>
+        /// <param name="bot">бот</param>
+        public async void Download(string chatid, string path, TelegramBotClient bot)
+        {
+            using (FileStream fs = new FileStream(path, FileMode.Open))
+            {
+                await bot.SendDocumentAsync(chatid, new Telegram.Bot.Types.InputFiles.InputOnlineFile(fs, path));
+            }
+        }
+
+        /// <summary>
+        /// Метод отправляет фото пользователю
+        /// </summary>
+        /// <param name="chatid">id чата</param>
+        /// <param name="path">Путь к файлу</param>
+        /// <param name="bot">бот</param>
+        public async void DownloadPhoto(string chatid, string path, TelegramBotClient bot)
+        {
+            using (FileStream fs = new FileStream(path, FileMode.Open))
+            {
+                await bot.SendPhotoAsync(chatid, new Telegram.Bot.Types.InputFiles.InputOnlineFile(fs, path));
+            }
+        }
+
+        /// <summary>
+        /// Метод показывает перечень команд
+        /// </summary>
+        /// <param name="user_name"></param>
+        /// <returns></returns>
+        public string Start(string user_name)
+        {
+            string result = $"Привет, {user_name}!\n\n" +
+                $"- Если Вы хотите загрузить файл, то просто отправьте его" +
+                $"\n- Если Вы хотите скачать файл выберите /download_files" +
+                $"\n- Для просмотра списка загруженных файлов выберите /list_files";
+            return result;
+        }
+
+        /// <summary>
+        /// Дессериализация файла со списком ранее загруженных файлов
+        /// </summary>
+        /// <param name="file_base"></param>
+        /// <returns></returns>
+        public List<string> DeSerialization()
+        {
+            List<string> file_base = new List<string>();
+
+            try // Если бот открывается первый раз и такого файла нет
+            {
+                string json = File.ReadAllText("base.json"); // Считать список загруженных ранее файлов
+                file_base = JsonConvert.DeserializeObject<List<string>>(json);
+                return file_base;
+            }
+            catch
+            {
+                return file_base;
+            }
+        }
+
+        /// <summary>
+        /// Сериализация файла со списком загруженных файлов
+        /// </summary>
+        /// <param name="list_files">Список загруженных файлов</param>
+        public void Serialization(List<string> list_files)
+        {
+            string json = JsonConvert.SerializeObject(list_files);
+            File.WriteAllText("base.json", json);
+        }
+
+        /// <summary>
+        /// Метод показывает список ранее загруженных файлов
+        /// </summary>
+        /// <param name="list_files"></param>
+        /// <returns></returns>
+        public string ShowListFiles(List<string> list_files)
+        {
+            string result = $"Список ранее загруженных файлов:\n";
+
+            for (int i = 0; i<list_files.Count; i++)
+                result += $"\n{i + 1}. {list_files[i]}";
+           
+            return result;
+        }
+
+        /// <summary>
+        /// Метод показывает список файлов для скачивания
+        /// </summary>
+        /// <param name="list_files">Список ранее загруженных файлов</param>
+        /// <returns></returns>
+        public string ShowListFilesForDownload(List<string> list_files)
+        {
+            string result = $"Для скачивание нажмите на файл:\n";
+
+            for (int i = 0; i < list_files.Count; i++)
+                result += $"\n/file{i + 1}-{list_files[i]}";
+
+            return result;
+        }
+
+        /// <summary>
+        /// Метод находит номер файла из строки, в противном случае возвращает -1
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public int GettFileNumber (string message)
+        {
+            bool input; // Проверка ввода
+            int file_number; // Номер файла
+
+            input = int.TryParse(message.Remove(0, 5), out file_number);
+
+            if (input) return file_number - 1;
+            else return -1;
+        }
+    }
+}
